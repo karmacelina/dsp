f0938129e6ce069f2be994da06ad35db61e0a659
Laura Colon-Melendez <lauradelmar@gmail.com>
2016-09-11 23:23:09 -0400

    Solved list questions

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -3,18 +3,16 @@
 
 
 def match_ends(words):
+    """ 
+    Given a list of strings, return the count of the number of 
+    strings where the string length is 2 or more and the first and 
+    last chars of the string are the same 
     """
-    Given a list of strings, return the count of the number of strings
-    where the string length is 2 or more and the first and last chars
-    of the string are the same.
-
-    >>> match_ends(['aba', 'xyz', 'aa', 'x', 'bbb'])
-    3
-    >>> match_ends(['', 'x', 'xy', 'xyx', 'xx'])
-    2
-    >>> match_ends(['aaa', 'be', 'abc', 'hello'])
-    1
-    """
+    count = 0
+    for word in words:
+        if len(word) >= 2 and word[0] == word[len(word)-1]:
+            count += 1
+    return count
     raise NotImplementedError
 
 
@@ -24,14 +22,22 @@ def front_x(words):
     order, except group all the strings that begin with 'x' first.
     e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
          ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'].
-
-    >>> front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa'])
-    ['xaa', 'xzz', 'axx', 'bbb', 'ccc']
-    >>> front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa'])
-    ['xaa', 'xcc', 'aaa', 'bbb', 'ccc']
-    >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
-    ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
+    new_list = []
+    list_x = []
+    for word in words:
+        if word[0] == "x":
+            list_x.append(word)
+        else:
+            new_list.append(word)
+    
+    list_x = sorted(list_x)
+    new_list = sorted(new_list)
+    
+    list_x.extend(new_list)
+    
+    return list_x
+
     raise NotImplementedError
 
 
@@ -41,14 +47,17 @@ def sort_last(tuples):
     increasing order by the last element in each tuple.
     e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
          [(2, 2), (1, 3), (3, 4, 5), (1, 7)].
-
-    >>> sort_last([(1, 3), (3, 2), (2, 1)])
-    [(2, 1), (3, 2), (1, 3)]
-    >>> sort_last([(2, 3), (1, 2), (3, 1)])
-    [(3, 1), (1, 2), (2, 3)]
-    >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
-    [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
+    reversed_tuple =[]
+    for tuple in tuples:
+        reversed_tuple.append(tuple[::-1])
+
+    new_list = []
+    for tuple in sorted(reversed_tuple):
+        new_list.append(tuple[::-1])
+    
+    return new_list
+
     raise NotImplementedError
 
 
@@ -58,17 +67,20 @@ def remove_adjacent(nums):
     elements have been reduced to a single element, so [1, 2, 2, 3]
     returns [1, 2, 3]. You may create a new list or modify the passed
     in list.
-
-    >>> remove_adjacent([1, 2, 2, 3])
-    [1, 2, 3]
-    >>> remove_adjacent([2, 2, 3, 3, 3])
-    [2, 3]
-    >>> remove_adjacent([3, 2, 3, 3, 3])
-    [3, 2, 3]
-    >>> remove_adjacent([])
-    []
     """
+    nums2 = list(nums) # copy the list
+
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i-1]: 
+            nums2[i] = ""
+    
+    while "" in nums2:
+        nums2.remove("")
+    
+    return nums2
+
     raise NotImplementedError
+
 
 
 def linear_merge(list1, list2):
@@ -85,4 +97,26 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
+    
+    
+    ## Not linear in time:
+    # list3 = list1 + list2
+    # return sorted(list3)
+    
+    ## Importing a module that called the merge algorithm:
+    # from heapq import merge 
+    # list3 = list(merge(list1, list2))
+    # return list3
+
+    list3 = []
+    while len(list1) and len(list2):
+        if list1[0] < list2[0]:
+            list3.append(list1.pop(0))
+        else:
+            list3.append(list2.pop(0))
+    list3.extend(list1)
+    list3.extend(list2)
+
+    return list3
+
     raise NotImplementedError
