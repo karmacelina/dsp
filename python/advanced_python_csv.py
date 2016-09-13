aee6448d3bd989e94263deb670111c850c4129d0
Laura Colon-Melendez <lauradelmar@gmail.com>
2016-09-13 09:50:54 -0400

    Created csv with emails

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,10 @@
-PLACE YOUR CODE HERE
+import pandas as pd 
+
+path = 'faculty.csv'
+faculty = pd.read_csv(path)
+
+df_emails = pd.DataFrame()
+
+df_emails['email'] = faculty[u' email']
+
+df_emails.to_csv('emails.csv', header = False, index = False)
